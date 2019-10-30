2de4ca73faf0950482ce9a9bc7aa8893ae0d5776
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2019-10-30 13:14:30 -0400

    HTTP and URL Logger

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 const express = require('express'); // importing a CommonJS module
+const helmet = require('helmet'); // Security Package
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
+// The three amigos
+// function dateLogger(req, res, next) {
+//   console.log (new Date().toISOString());
+// }
+
+//HTTP and URL Logger
+function historyLogger(req, res, next) {
+  console.log (`${req.method} to ${req.url}`);
+
+  next();
+}
+
 server.use(express.json());
 
+// Global middleware (runs on ever request that comes into the server)
+server.use(helmet())
 server.use('/api/hubs', hubsRouter);
+server.use(historyLogger);
 
 server.get('/', (req, res) => {
   const nameInsert = (req.name) ? ` ${req.name}` : '';
